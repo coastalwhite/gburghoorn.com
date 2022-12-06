c2eb56bba6c88ee572d067efd0ed2b838bb91ba2
Gijs Burghoorn <g.burghoorn@gmail.com>
2022-12-06 14:35:02 +0100

    Update the building workflow

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ["./templates/**/*.html"],
+  content: ["./templates/**/*.html"],
   theme: {
     extend: {
       fontSize: {
