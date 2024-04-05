fae0ac5756a97c1271ecfd5761b1a944f715b7d1
coastalwhite <me@gburghoorn.com>
2024-04-05 11:56:57 +0200

    Add a proper devShell

--- flake.nix
+++ flake.nix
@@ -1,27 +1,36 @@
 {
-    description = "Personal Website";
-
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs";
-    };
-
-    outputs = { nixpkgs, ... }: 
-    let
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-    in {
-        packages.${system}.default = pkgs.stdenv.mkDerivation {
-            version = "0.1";
-            pname = "gburghoorn.com";
-
-            src = ./.;
-
-            buildPhase = ''
-                ${pkgs.tailwindcss}/bin/tailwindcss -i styles/styles.scss -o static/styles/styles.css --minify
-                ${pkgs.zola}/bin/zola build
-                mkdir -p $out
-                cp -r public/* $out/
-            '';
+  description = "Personal Website";
+  
+  inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs";
+      utils.url = "github:numtide/flake-utils";
+  };
+  
+  outputs = { self, nixpkgs, utils }: 
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          version = "0.1";
+          pname = "gburghoorn.com";
+      
+          src = ./.;
+      
+          buildPhase = ''
+            ${pkgs.tailwindcss}/bin/tailwindcss -i styles/styles.scss -o static/styles/styles.css --minify
+            ${pkgs.zola}/bin/zola build
+            mkdir -p $out
+            cp -r public/* $out/
+          '';
         };
-    };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            tailwindcss
+            zola
+          ];
+        };
+      }
+    );
 }
